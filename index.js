966b15adf713844cf473335f3ebf09a7c4204be2
Akeia M <mccollumakeia@outlook.com>
2022-01-14 09:24:33 -0500

    created an array of questions for user input

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        name: 'github', 
+        type: 'input',
+        message: 'What is your github username?'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'What is your email address?'
+    },
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of your project?'
+        }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
